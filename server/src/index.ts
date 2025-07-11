848d1be253cc3794b91578c180207abe9edb6b86
SK <gfawkes1832@gmail.com>
2025-07-11 13:53:31 +0530

    updated multer configuration to store files correctly. also tested w/ postman - works good!

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import uploadRouter from './routes/upload';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(cors());
 app.use('/api/upload', uploadRouter);
 
 app.listen(PORT, () => {
--- server/src/routes/upload.ts
+++ server/src/routes/upload.ts
@@ -1,7 +1,30 @@
 import { Router } from "express"
+import multer from "multer";
+import path from "path"
 
 const router = Router();
-router.post('/', (req, res) => {
+
+const storage = multer.diskStorage({
+  destination: function (req, res, cb) {
+    cb(null, 'uploads/')
+  },
+
+  filename: function (req, file, cb) {
+    const suffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    const extension = path.extname(file.originalname);
+    cb(null, file.fieldname + '-' + suffix + extension);
+  }
+})
+
+const upload = multer({ storage });
+
+router.post('/', upload.single('file'), (req, res) => {
+  if (!req.file) {
+    res.json({ message: 'No file upload found' });
+  }
+
+  console.log(req.body, req.file);
+
   res.json({ message: 'File uploaded successfully' })
 })
 
