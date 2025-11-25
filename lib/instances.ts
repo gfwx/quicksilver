f7eba67371b95d36a86ea25de99adae2b680c065
gfwx <gfawkes1832@gmail.com>
2025-11-25 13:47:17 +0530

    updates to prisma

--- lib/instances.ts
+++ lib/instances.ts
@@ -1,11 +1,20 @@
 import { Prisma, PrismaClient } from "@prisma/client";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import { WorkOS } from "@workos-inc/node";
 // This is done to prevent additional PrismaClient instances from being created upon mounting
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+const adapter = new PrismaBetterSqlite3({
+  database: process.env.DATABASE_URL,
+});
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  });
 type ModelNames = Prisma.ModelName;
 export type PrismaModels = {
   [M in ModelNames]: Exclude<
